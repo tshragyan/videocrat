48763bd276f22c0b1da18a1235e67f09c64e36bb
Daniel <dchragyan1@gmail.com>
2026-08-10 17:57:55 +0400

    add logs models

--- app/Models/ShopifyErrorLog.php
+++ app/Models/ShopifyErrorLog.php
@@ -4,8 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class ShopifyErrorLog
+ * @package App\Models
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $method
+ * @property int $data
+ * @property User $user
+ */
 class ShopifyErrorLog extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'method',
+        'data',
+    ];
+
+    public static array $columns = [
+        'id',
+        'user_id',
+        'method',
+        'data',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
