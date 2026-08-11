d1740564d570dc22bef4e539c5c87df27b8610b9
Daniel <dchragyan1@gmail.com>
2026-08-11 11:21:02 +0400

    add shopify service

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     function index()
     {
-        return true;
+        return view('app');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('app');
-});
-
 Route::middleware('shopify.host')->group(function() {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard.home');
     Route::prefix('auth')->name('shopify.')->group(function() {
