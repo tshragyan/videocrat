7e581712d5b17e0f770c4c3c83e06109b74f0a69
Daniel <dchragyan1@gmail.com>
2026-08-17 10:01:25 +0400

    add navigation

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppProvider, Page, Card, Text } from '@shopify/polaris';
+import { AppProvider as PolarisRouter } from '@shopify/shopify-app-react-router/react';
 
 import '@shopify/polaris/build/esm/styles.css';
-
 function App() {
     return (
         <AppProvider i18n={{}}>
+            <s-app-nav>
+                <a href="/app">Home</a>
+                <a href="/app/videos">Videos</a>
+                <a href="/app/products">Products</a>
+                <a href="/app/settings">Settings</a>
+            </s-app-nav>
             <Page title="Videocrat">
                 <Card>
                     <Text as="p" variant="bodyMd">
