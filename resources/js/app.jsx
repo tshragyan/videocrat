6c362fcda3686187c87bf62e58316bd9c35ed9c9
Daniel <dchragyan1@gmail.com>
2026-08-17 16:35:43 +0400

    adding routes

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -29,6 +29,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<AppLayout />}>
+                        <Route index element={<Home />} />
                         <Route path="app/" element={<Home />} />
                         <Route path="app/videos" element={<Videos />} />
                         <Route path="app/settings" element={<Settings />} />
